11d5df2e19ad19432ce2528b11de082c51674182
Mark Kuczmarski <5359580+kuczmama@users.noreply.github.com>
2023-09-16 16:25:54 -0700

    Be able to autoplay video on repeat

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import os
 import pygame
 import hashlib
 import streamlit as st
+import time
+import base64
 
 def get_sha256(text):
     """Compute the SHA-256 hash of the text and return it."""
     return hashlib.sha256(text.encode()).hexdigest()
 
-def speak_text(text):
+# Given some arbitrary text, create an 
+# audio file that can play the text
+def create_audio_file(text):
     CHUNK_SIZE = 1024
     VOICE_ID = "LcfcDJNUP1GQjkzn1xUU"
     url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
@@ -47,14 +51,27 @@ def speak_text(text):
 
     return filename
 
-st.title("Retrograde Window Audio Player")
+def file_to_data_url(file_path):
+    """Convert a file to its corresponding data URL."""
+    ext = os.path.splitext(file_path)[1][1:]  # Extract file extension without dot
+    with open(file_path, "rb") as f:
+        data = f.read()
+        base64_data = base64.b64encode(data).decode()
+        return f"data:audio/{ext};base64,{base64_data}"
+
+st.title("Help prompt the user")
 
 # UI input
 memory_span = st.number_input("Memory Span")
 
-if st.button("Play Audio"):
-    audio_file = speak_text(f"You entered retrograde window value: {memory_span}")
-    st.audio(audio_file, format='audio/mp3')
+start_speaking = st.button("Start Speaking")
 
-if __name__ == "__main__":
-    st.write("Enter a number for the retrograde window and press 'Play Audio' to hear it.")
+if start_speaking:
+    while True:
+        audio_file_path = create_audio_file(f"All things are good")
+        data_url = file_to_data_url(audio_file_path)
+        html_string = f"<audio controls autoplay><source src='{data_url}' type='audio/mp3'></audio>"
+        sound = st.empty()
+        sound.markdown(html_string, unsafe_allow_html=True)
+        time.sleep(2)
+        sound.empty()
