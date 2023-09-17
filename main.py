2d3b76ea1ea065a907f7f9b48cff421b4999222d
Mark Kuczmarski <5359580+kuczmama@users.noreply.github.com>
2023-09-16 18:24:08 -0700

    Be able to call chatgpt and generate time intervals

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 import requests
 import os
 import pygame
+import openai
 import hashlib
 import streamlit as st
 import time
 import base64
 from mutagen.mp3 import MP3
+import json
+from datetime import datetime, timedelta
+
+ELEVEN_LABS_API_KEY = os.environ['ELEVEN_LABS_API_KEY']
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_NAME = 'gpt-3.5-turbo'
+VOICE_ID = "LcfcDJNUP1GQjkzn1xUU" # Emily
+# VOICE_ID = "zcAOhNBS3c14rBihAFp1" # swedish
+# VOICE_ID = "oM29XZNJ7O9aApNNeSVY" # Paul
 
 def get_audio_length(file_path):
     """Get the duration of an audio file in seconds."""
@@ -21,12 +31,7 @@ def get_sha256(text):
 # audio file that can play the text
 def create_audio_file(text):
     CHUNK_SIZE = 1024
-    # VOICE_ID = "zcAOhNBS3c14rBihAFp1" # swedish
-    VOICE_ID = "LcfcDJNUP1GQjkzn1xUU" # Emily
-    # VOICE_ID = "oM29XZNJ7O9aApNNeSVY" # Paul
     url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
-
-    ELEVEN_LABS_API_KEY = os.environ['ELEVEN_LABS_API_KEY']
 
     headers = {
         "Accept": "audio/mpeg",
@@ -69,22 +74,77 @@ def file_to_data_url(file_path):
         base64_data = base64.b64encode(data).decode()
         return f"data:audio/{ext};base64,{base64_data}"
 
+def get_text_from_time(input_time):
+    # Load the prompts from the JSON file
+    with open('prompts.json', 'r') as file:
+        prompts = json.load(file)
+
+    # Convert the list of dictionaries into a single dictionary
+    # with the time as the key and the message as the value
+    time_message_dict = {entry['time']: entry['message'] for entry in prompts}
+
+    # Iterate through the dictionary keys to find the corresponding message
+    for time, prompt in time_message_dict.items():
+        # if the input time is less than or equal to the current time in the iteration, return the corresponding prompt
+        if input_time <= time:
+            return prompt
+    
+    # Return a default message if the input time doesn't match any key in the dictionary
+    return "Time not found in predefined prompts."
+
+# Example
+
 print("Creating audio file")
-create_audio_file(f"Hey mom! Everything is ok, you are right where you should be.")
 
 st.title("Help prompt the user")
 
 # UI input
-memory_span = st.number_input("Memory Span")
+memory_span = st.number_input("Memory Span (minutes)")
+schedule = st.text_area("Schedule")
 
 start_speaking = st.button("Start Speaking")
 
+def call_chatgpt(content):
+    print("Calling chatGPT: ", content)
+    completion = openai.ChatCompletion.create(model=MODEL_NAME, messages=[{
+    "role": "user", 
+    "content": content}])
+    result = completion.choices[0].message.content
+    print("completion: ", result)
+    return result
+
+call_chatgpt("Hello how are you?")
+
+def generate_intervals(n, start_time='6:00am', end_time='8:00am'):
+    # Convert start and end times to datetime objects
+    start = datetime.strptime(start_time, '%I:%M%p')
+    end = datetime.strptime(end_time, '%I:%M%p')
+    
+    intervals = []
+    current_time = start
+
+    while current_time <= end:
+        intervals.append(current_time.strftime('%I:%M%p').lstrip('0'))  # Format and remove leading zeros
+        current_time += timedelta(minutes=n)
+
+    return intervals
+
+# Generate an interval to prompt the user every n minutes
+times = generate_intervals(memory_span)
+
 if start_speaking:
-    audio_file_path = create_audio_file(f"Hey mom! Everything is ok, you are right where you should be.")
-    duration = get_audio_length(audio_file_path)
-    data_url = file_to_data_url(audio_file_path)
-    html_string = f"<audio controls autoplay><source src='{data_url}' type='audio/mp3'></audio>"
-    sound = st.empty()
-    sound.markdown(html_string, unsafe_allow_html=True)
-    time.sleep(duration + 1)  # Adding 1 second to ensure the audio finishes playing
-    sound.empty()
+    current_time = st.title(f"Current time: {times[0]}")
+    for time_str in times:
+        current_time.empty()
+        current_time = st.title(f"Current time: {time_str}")
+        text = get_text_from_time(time_str)
+        audio_file_path = create_audio_file(text)
+        audio_length = get_audio_length(audio_file_path)
+        data_url = file_to_data_url(audio_file_path)
+        html_string = f"<audio controls autoplay><source src='{data_url}' type='audio/mp3'></audio>"
+        sound = st.empty()
+        sound.markdown(html_string, unsafe_allow_html=True)
+        print(f"duration {int(audio_length) + 1}")
+        time.sleep(int(audio_length) + 1)  # Adding 1 second to ensure the audio finishes playing
+        sound.empty()
+        # time.sleep(memory_span * 60)
